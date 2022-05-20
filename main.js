5e7546d5575d567db199b67d9763537a5d9096a0
Naadii <96473790+Naadii@users.noreply.github.com>
2022-05-21 01:10:13 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const BASE_URL = "https://ptf-web-dizajn-2022.azurewebsites.net/"
 
+let services = [];
+
 fetch(`${BASE_URL}/api/Services`)
     .then(response => {
+        if(!response.ok){
+            throw Error('Error');
+        }
         return response.json();
     })
     .then(data => {
@@ -22,10 +27,54 @@ const renderServices = (services) => {
                 <div class="carousel-caption d-none d-md-block">
                      <h1>${service.name}</h1>
                      <h2>Price: ${service.price} $</h2>
+                     <button type="button" onclick="fillEditData(${service.id})" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal1" data-bs-whatever="@getbootstrap">Edit</button>
                 </div>
             </div>
         </div>`;
     });
 
     servicesId.innerHTML = resultHtml;
+}
+
+const fillEditData = (serviceId) => {
+    const service = services.find(service => service.id === serviceId);
+    const serviceFormId = document.getElementById('service-id');
+    const serviceFormName = document.getElementById('service-name');
+    const serviceFormPrice = document.getElementById('service-price');
+    const serviceFormPhoto = document.getElementById('service-photo');
+    
+    serviceFormId.value = service.id;
+    serviceFormName.value = service.name;
+    serviceFormPrice.value = service.price;
+    serviceFormPhoto.value = service.photoUrl;
+}
+
+const editServices = () => { 
+    const serviceFormId = document.getElementById('service-id').value;
+    const serviceFormName = document.getElementById('service-name').value;
+    const serviceFormPrice = document.getElementById('service-price').value;
+    const serviceFormPhoto = document.getElementById('service-photo').value;
+
+    fetch(`${BASE_URL}/api/Services`, {
+        method: 'PUT',
+        headers: new Headers ({'content-type': 'application/json'}),
+        body: JSON.stringify({
+            id: serviceFormId,
+            name: serviceFormName,
+            price: serviceFormPrice,
+            photoUrl: serviceFormPhoto
+            })
+        .then(response => {
+            if(!response.ok){
+                throw Error('Error');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    })
 }
